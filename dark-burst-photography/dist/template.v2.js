54fcd63a96e6c57c46b9e628f63a5906439858b2
erkuterdem <erkut@cs.hacettepe.edu.tr>
2021-11-20 13:28:20 +0300

    Update template.v2.js

--- dark-burst-photography/dist/template.v2.js
+++ dark-burst-photography/dist/template.v2.js
@@ -25,13 +25,13 @@
       <h3>Published</h3>
       ${e.publishedDate?`
         <p>${e.publishedMonth} ${e.publishedDay}, ${e.publishedYear}</p> `:`
-        <p><em>IEEE Transactions on Image Processing, in press.</em></p>`}
+        <p><em>IEEE Transactions on Image Processing, Vol 30, pg. 9372-9385, 2021. </em></p>`}
     </div>
     <div>
       <h3>DOI</h3>
       ${e.doi?`
         <p><a href="https://doi.org/${e.doi}">${e.doi}</a></p>`:`
-        <p><em>No DOI yet.</em></p>`}
+        <p><em>10.1109/TIP.2021.3125394</em></p>`}
     </div>
   </div>
 `}function x(e,t,n=document){if(0<t.size){e.style.display='';let i=e.querySelector('.references');if(i)i.innerHTML='';else{const t=n.createElement('style');t.innerHTML=Mi,e.appendChild(t);const a=n.createElement('h3');a.id='references',a.textContent='References',e.appendChild(a),i=n.createElement('ol'),i.id='references-list',i.className='references',e.appendChild(i)}for(const[e,a]of t){const t=n.createElement('li');t.id=e,t.innerHTML=o(a),i.appendChild(t)}}else e.style.display='none'}function k(e,t){let n=`
